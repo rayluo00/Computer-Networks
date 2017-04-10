36c45ce14e878e2fd130529ab5b3cbe52b54e646
Ray Luo <rayluo00@gmail.com>
2017-04-09 23:32:50 -0700

    adding client and server exec

--- Database/db_server.c
+++ Database/db_server.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <time.h>
 #include "database.h"
+#include <gdbm.h>
 
 int db_start () 
 {
@@ -13,6 +14,22 @@ int db_start ()
 
 int db_option (int option)
 {
-    printf("User chose option: %d\n", option);
+	switch (option) {
+		case 0:
+			printf("Forbidden area...\n");
+			break;
+		case 1:
+			printf("Create database...\n");
+			break;
+		case 2:
+			printf("Open database...\n");
+			break;
+		case 3:
+			printf("Close database...\n");
+			break;
+		default:
+			printf("Invalid option.");
+	}	
+    
     return 0;
 }
